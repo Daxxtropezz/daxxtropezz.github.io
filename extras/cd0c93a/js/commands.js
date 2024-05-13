7f5ec89c15cb2d430841a40614ef8d9acae0b431
Daxxtropezz <miraflores.john@gmail.com>
2024-05-13 17:45:34 +0800

    update: terminal, added password and more commands. css renewed totally and fixed the font-family system

--- extras/cd0c93a/js/commands.js
+++ extras/cd0c93a/js/commands.js
@@ -1,81 +1,88 @@
-var twitter = "https://twitter.com/daxxtropezz";
-var linkedin = "https://www.linkedin.com/in/daxxtropezz/";
-var facebook = "https://www.fb.com/daxxtropezz/";
-var instagram = "https://www.instagram.com/daxxtropezz/";
-var github = "https://github.com/daxxtropezz";
-var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
-var emailme = "mailto:miraflores.john@gmail.com";
+var twitter = 'https://twitter.com/daxxtropezz'
+var linkedin = 'https://www.linkedin.com/in/daxxtropezz/'
+var facebook = 'https://www.fb.com/daxxtropezz/'
+var instagram = 'https://www.instagram.com/daxxtropezz/'
+var github = 'https://github.com/daxxtropezz'
+var sudo = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1'
+var emailme = 'mailto:miraflores.john@gmail.com'
+var password = 'daxxyishere'
 
 help = [
-  `<br><pre class="whitespace-pre-wrap">
-  <span class="command">back</span>\t\t&nbsp;\t\t\treturns to main portfolio
-  <span class="command">banner</span>\t\t&nbsp;\tdisplays the header for this terminal
-  <span class="command">clear</span>\t\t&nbsp;\t\t\tclears the terminal
-  <span class="command">cls</span>\t\t&nbsp;\t\t\t\tclears the terminal
-  <span class="command">emailme</span>\t\t&nbsp;\tdon't try to send me an email
-  <span class="command">help</span>\t\t&nbsp;\t\t\tprovides help information for commands
-  <span class="command">history</span>\t\t&nbsp;\t\tviews command history
-  <span class="command">projects</span>\t\t&nbsp;\tviews my projects
-  <span class="command">secret</span>\t\t&nbsp;\t\topens a secret command code
-  <span class="command">social</span> \t\t&nbsp;\t\tdisplays social networks
-  <span class="command">whoami</span>\t\t&nbsp;\tdiplays a description of who i am
-  </pre>`,
-];
+  '<br>',
+  '<span class="command">back</span>              returns to main portfolio',
+  '<span class="command">banner</span>            displays the header for this terminal',
+  '<span class="command">clear</span>             clears the terminal',
+  '<span class="command">cls</span>               clears the terminal',
+  '<span class="command">emailme</span>           don\'t try to send me an email',
+  '<span class="command">help</span>              provides help information for commands',
+  '<span class="command">history</span>           views command history',
+  '<span class="command">projects</span>          views my projects',
+  '<span class="command">secret</span>            opens a secret command code',
+  '<span class="command">social</span>            displays social networks',
+  '<span class="command">whoami</span>            diplays a description of who i am',
+  '<br>',
+]
+
+secret = [
+  '<br>',
+  '<span class="command">sudo</span>           a secret code for ADMIN ONLY<br>',
+  '<span class="command">password</span>       don\'t try it. you already broke the code!!!',
+  '<br>',
+]
 
 whoami = [
-  "<br>",
-  "<span>Hey, I'm Daxxtropezz!👋</span>",
-  '🔭 I’m a graduate student from CvSU - Imus Campus, with a degree of <span class="command text-[#75e1e7]">Bachelor of Science in Information Technology</span>.',
-  `<ul>🌱 My fields of intetests are:<li>Fullstack Development</li><li>UI/UX Designing</li></ul>`,
-  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
+  '<br>',
+  "<span>Hallo, I'm Daxxtropezz!👋</span>",
+  '🔭 I’m a graduate student from CvSU - Imus Campus, with a degree of <span class="command">Bachelor of Science in Information Technology</span>.',
+  `<ul>🌱 My fields of intetests are:
+  <li>       > Fullstack Development</li><li>       > UI/UX Designing</li></ul><pre>👯 I would love to work with anyone who wants to build a 
    professional website for their business or even a personal 
    website to showcase their portfolio.
    I love learning new stuff and using it to improve my skills.
    You can learn more about the projects that I have built using 
-   the <span class="command text-[#75e1e7]">'projects'</span> command.
+   the <span class="command">'projects'</span> command.
    You can browse around my website to find out more about 
-   me. Try using <span class="command text-[#75e1e7]">social commands</span> and/or <span class="command text-[#75e1e7]">'help'</span> to explore more.</pre>`,
-  `<li>👨‍💻 You can use the <span class="command text-[#75e1e7]">'github'</span> command to take a look at my GitHub profile.`,
-  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=miraflores.john@gmail.com">miraflores.john@gmail.com</a>`,
-  "<br>",
-];
+   me. Try using <span class="command">social commands</span> and/or <span class="command">'help'</span> to explore more.</pre>`,
+  `<li>👨‍💻 You can use the <span class="command">'github'</span> command to take a look at my GitHub profile.</li>`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=miraflores.john@gmail.com">miraflores.john@gmail.com</a></li>`,
+  '<br>',
+]
 
 // prettier-ignore
 social = [
-    "<br><pre class='whitespace-pre-wrap'>",
-    'typing them as command also opens their link in new tab:',
-    'twitter&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\t\t\t\t<a href="' + twitter + '" target="blank">twitter/daxxtropezz</a>',
-    'linkedin&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\t\t\t\t<a href="' + linkedin + '" target="blank">linkedin/daxxtropezz</a>',
-    'facebook&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\t\t\t\t<a href="' + facebook + '" target="blank">facebook/daxxtropezz</a>',
-    'instagram&nbsp;&nbsp;&nbsp;&nbsp;\t\t\t\t<a href="' + instagram + '" target="blank">instagram/daxxtropezz</a>',
-    'github&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\t\t\t\t<a href="' + github + '" target="blank">github/daxxtropezz</a>',
-    "</pre><br>",
-];
+    '<br>',
+    '<span class="command">can be used as command:</span>',
+    'twitter            <a href="' + twitter + '" target="blank">twitter/daxxtropezz</a>',
+    'linkedin           <a href="' + linkedin + '" target="blank">linkedin/daxxtropezz</a>',
+    'facebook           <a href="' + facebook + '" target="blank">facebook/daxxtropezz</a>',
+    'instagram          <a href="' + instagram + '" target="blank">instagram/daxxtropezz</a>',
+    'github             <a href="' + github + '" target="blank">github/daxxtropezz</a>',
+    '<br>',
+]
 
 projects = [
-  `<br><pre class="whitespace-pre-wrap">
-  Still curating... most projects are offline, on GitHub, or confidential.
-  </pre>`,
-];
+  '<br>',
+  'Still curating... most projects are offline, on GitHub, or confidential.',
+  '<br>',
+]
 
 banner = [
-  `<pre
-  class="ascii" style="font-family: monospace,-webkit-pictograph;">
-  
-  ░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░
-  ░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗
-  ░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║
-  ░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║
-  ░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝
-  ░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░ © 2023
-  ██████╗░░█████╗░██╗░░██╗██╗░░██╗██╗░░░██╗██╗░██████╗  ░██╗░░░░░░░██╗░█████╗░██████╗░██╗░░░░░██████╗░
-  ██╔══██╗██╔══██╗╚██╗██╔╝╚██╗██╔╝╚██╗░██╔╝╚█║██╔════╝  ░██║░░██╗░░██║██╔══██╗██╔══██╗██║░░░░░██╔══██╗
-  ██║░░██║███████║░╚███╔╝░░╚███╔╝░░╚████╔╝░░╚╝╚█████╗░  ░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░░░░██║░░██║
-  ██║░░██║██╔══██║░██╔██╗░░██╔██╗░░░╚██╔╝░░░░░░╚═══██╗  ░░████╔═████║░██║░░██║██╔══██╗██║░░░░░██║░░██║
-  ██████╔╝██║░░██║██╔╝╚██╗██╔╝╚██╗░░░██║░░░░░░██████╔╝  ░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║███████╗██████╔╝
-  ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░░░╚═════╝░  ░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░
-  </pre>`,
-  `<div class="pt-2"><span class="text-[#7d82d7db]">Welcome to my interactive web terminal! — Type <span class="command text-[#75e1e7]">'help'</span> for a list of supported commands.</span></div>`,
-  `Or you can click <a onclick="history.back()" target="blank">return</a> to go back to the main portfolio.`,
-  `<span class="command text-[#75e1e7]">Created by Daxxtropezz. All rights reserved.</span>`,
-];
+  '<br>',
+  '░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░',
+  '░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗',
+  '░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║',
+  '░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║',
+  '░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝',
+  '░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░ © 2023',
+  '██████╗░░█████╗░██╗░░██╗██╗░░██╗██╗░░░██╗██╗░██████╗  ░██╗░░░░░░░██╗░█████╗░██████╗░██╗░░░░░██████╗░',
+  '██╔══██╗██╔══██╗╚██╗██╔╝╚██╗██╔╝╚██╗░██╔╝╚█║██╔════╝  ░██║░░██╗░░██║██╔══██╗██╔══██╗██║░░░░░██╔══██╗',
+  '██║░░██║███████║░╚███╔╝░░╚███╔╝░░╚████╔╝░░╚╝╚█████╗░  ░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░░░░██║░░██║',
+  '██║░░██║██╔══██║░██╔██╗░░██╔██╗░░░╚██╔╝░░░░░░╚═══██╗  ░░████╔═████║░██║░░██║██╔══██╗██║░░░░░██║░░██║',
+  '██████╔╝██║░░██║██╔╝╚██╗██╔╝╚██╗░░░██║░░░░░░██████╔╝  ░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║███████╗██████╔╝',
+  '╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░░░╚═════╝░  ░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░',
+  '<br>',
+  'Welcome to my interactive web terminal! — Type <span class="command">\'help\'</span> for a list of supported commands.',
+  'Or you can click <a onclick="history.back()" target="blank">return</a> to go back to the main portfolio.',
+  '<span class="command">Created by Daxxtropezz. All rights reserved.</span>',
+  '<br>',
+]
--- extras/cd0c93a/js/main.js
+++ extras/cd0c93a/js/main.js
@@ -1,190 +1,237 @@
-var before = document.getElementById("before");
-var liner = document.getElementById("liner");
-var command = document.getElementById("typer");
-var textarea = document.getElementById("texter");
-var terminal = document.getElementById("terminal");
+var before = document.getElementById('before')
+var liner = document.getElementById('liner')
+var command = document.getElementById('typer')
+var textarea = document.getElementById('texter')
+var terminal = document.getElementById('terminal')
 
-var git = 0;
-var pw = false;
-var commands = [];
+var git = 0
+var pw = false
+let pwd = false
+var commands = []
 
 setTimeout(function () {
-  loopLines(banner, "", 80);
-  textarea.focus();
-}, 100);
+  loopLines(banner, '', 80)
+  textarea.focus()
+}, 100)
 
-window.addEventListener("keyup", enterKey);
+window.addEventListener('keyup', enterKey)
 
 //init
-textarea.value = "";
-command.innerHTML = textarea.value;
+textarea.value = ''
+command.innerHTML = textarea.value
 
 function enterKey(e) {
   if (e.keyCode == 181) {
-    document.location.reload(true);
+    document.location.reload(true)
   }
-
-  if (e.keyCode == 13) {
-    commands.push(command.innerHTML);
-    git = commands.length;
-    addLine(
-      "guest@daxxtropezz.github.io:~$⠀" + command.innerHTML,
-      "no-animation",
-      0
-    );
-    commander(command.innerHTML.toLowerCase());
-    command.innerHTML = "";
-    textarea.value = "";
-  }
-  if (e.keyCode == 38 && git != 0) {
-    git -= 1;
-    textarea.value = commands[git];
-    command.innerHTML = textarea.value;
-  }
-  if (e.keyCode == 40 && git != commands.length) {
-    git += 1;
-    if (commands[git] === undefined) {
-      textarea.value = "";
-    } else {
-      textarea.value = commands[git];
+  if (pw) {
+    let et = '*'
+    let w = textarea.value.length
+    command.innerHTML = et.repeat(w)
+    if (textarea.value === password) {
+      pwd = true
     }
-    command.innerHTML = textarea.value;
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, 'color2 margin', 120)
+      command.innerHTML = ''
+      textarea.value = ''
+      pwd = false
+      pw = false
+      liner.classList.remove('password')
+    } else if (e.keyCode == 13) {
+      addLine('Wrong password', 'error', 0)
+      command.innerHTML = ''
+      textarea.value = ''
+      pw = false
+      liner.classList.remove('password')
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML)
+      git = commands.length
+      addLine(
+        'guest@daxxtropezz.github.io:~$⠀' + command.innerHTML,
+        'no-animation',
+        0,
+      )
+      commander(command.innerHTML.toLowerCase())
+      command.innerHTML = ''
+      textarea.value = ''
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1
+      textarea.value = commands[git]
+      command.innerHTML = textarea.value
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1
+      if (commands[git] === undefined) {
+        textarea.value = ''
+      } else {
+        textarea.value = commands[git]
+      }
+      command.innerHTML = textarea.value
+    }
   }
 }
 
 function commander(cmd) {
   switch (cmd.toLowerCase()) {
-    case "help":
-      loopLines(help, "color2 margin", 80);
-      break;
-    case "back":
-      addLine("returning to main profile...", "color2", 80);
+    case 'help':
+      loopLines(help, 'color2 margin', 80)
+      break
+    case 'back':
+      addLine('returning to main profile...', 'color2', 80)
       setTimeout(function () {
-        window.open("../../", "_self");
-      }, 500);
-      break;
-    case "whoami":
-      loopLines(whoami, "color2 margin", 80);
-      break;
-    case "social":
-      loopLines(social, "color2 margin", 80);
-      break;
-    case "projects":
-      loopLines(projects, "color2 margin", 80);
-      break;
-    case "history":
-      addLine("<br>", "", 0);
-      loopLines(commands, "color2", 80);
-      addLine("<br>", "command", 80 * commands.length + 50);
-      break;
-    case "emailme":
+        window.open('../../', '_self')
+      }, 500)
+      break
+    case 'whoami':
+      loopLines(whoami, 'color2 margin', 80)
+      break
+    case 'social':
+      loopLines(social, 'color2 margin', 80)
+      break
+    case 'projects':
+      loopLines(projects, 'color2 margin', 80)
+      break
+    case 'history':
+      addLine('<br>', '', 0)
+      loopLines(commands, 'color2', 80)
+      addLine('<br>', 'command', 80 * commands.length + 50)
+      break
+    case 'emailme':
       addLine(
-        'opening mailto:<a href="mailto:miraflores.john@gmail.com"> miraflores.john@gmail.com</a>...',
-        "color2",
-        80
-      );
-      newTab(emailme);
-      break;
-    case "clear":
-    case "cls":
+        `<br>
+        opening mailto:<a href="mailto:miraflores.john@gmail.com"> miraflores.john@gmail.com</a>...
+        <br>
+        `,
+        'color2',
+        80,
+      )
+      newTab(emailme)
+      break
+    case 'clear':
+    case 'cls':
       setTimeout(function () {
-        terminal.innerHTML = '<a id="before"></a>';
-        before = document.getElementById("before");
-      }, 1);
-      break;
-    case "banner":
-      loopLines(banner, "", 80);
-      break;
-    case "secret":
+        terminal.innerHTML = '<a id="before"></a>'
+        before = document.getElementById('before')
+      }, 1)
+      break
+    case 'banner':
+      loopLines(banner, '', 80)
+      break
+    case 'secret':
+      liner.classList.add('password')
+      pw = true
+      break
+    case 'password':
       addLine(
-        `<br><pre class="whitespace-pre-wrap">\t<span class="command">sudo</span>\t\t&nbsp;\ta secret code for ADMIN ONLY
-        </pre>`,
-        "color2",
-        0
-      );
-      break;
+        `<br>
+        do you even know what you're doing?
+        <br>
+        `,
+        'color2',
+        0,
+      )
+      break
+    case 'sudo':
+    case 'sudo su':
+      addLine(
+        `<br>
+        oh no, you're not an admin...
+        <br>
+        `,
+        'color2',
+        0,
+      )
+      newTab(sudo)
+      break
     // socials
-    case "facebook":
-    case "fb":
-      addLine("opening Facebook...", "color2", 0);
-      newTab(facebook);
-      break;
-    case "twitter":
-      addLine("opening Twitter...", "color2", 0);
-      newTab(twitter);
-      break;
-    case "linkedin":
-      addLine("opening LinkedIn...", "color2", 0);
-      newTab(linkedin);
-      break;
-    case "instagram":
-    case "ig":
-      addLine("opening Instagram...", "color2", 0);
-      newTab(instagram);
-      break;
-    case "github":
-    case "git":
-      addLine("opening GitHub...", "color2", 0);
-      newTab(github);
-      break;
-    case "sudo":
-    case "sudo su":
-      addLine("oh no, you're not an admin...", "color2", 0);
-      newTab(sudo);
-      break;
+    case 'facebook':
+    case 'fb':
+      addLine('opening Facebook...', 'color2', 0)
+      newTab(facebook)
+      break
+    case 'twitter':
+      addLine('opening Twitter...', 'color2', 0)
+      newTab(twitter)
+      break
+    case 'linkedin':
+      addLine('opening LinkedIn...', 'color2', 0)
+      newTab(linkedin)
+      break
+    case 'instagram':
+    case 'ig':
+      addLine('opening Instagram...', 'color2', 0)
+      newTab(instagram)
+      break
+    case 'github':
+    case 'git':
+      addLine('opening GitHub...', 'color2', 0)
+      newTab(github)
+      break
     default:
       addLine(
         // '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'help\'</span>.</span>',
-        `<span>Command not found. For a list of commands, type <span class="command">'help'</span>.</span>
-        <br><span> <b>Note</b>: Press <span class = "command">'↳ Tab'</span> key or click <b class="cursor" id="cursor">█</b> if you cannot type</span>`,
-        "error",
-        100
-      );
-      break;
+        `<br>
+        Command not found. For a list of commands, type <span class="command">'help'</span>.
+        <br>
+        <b>Note</b>: Press <span class = "command">'↳ Tab'</span> key or click <b class="cursor" id="cursor">█</b> if you cannot type.
+        <br>
+        `,
+        'error',
+        100,
+      )
+      break
   }
 }
 
 function newTab(link) {
   setTimeout(function () {
-    window.open(link, "_blank");
-  }, 500);
+    window.open(link, '_blank')
+  }, 500)
 }
 
 function addLine(text, style, time) {
-  var t = "";
+  var t = ''
   for (let i = 0; i < text.length; i++) {
-    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
-      t += "&nbsp;&nbsp;";
-      i++;
+    if (text.charAt(i) == ' ' && text.charAt(i + 1) == ' ') {
+      t += '&nbsp;&nbsp;'
+      i++
     } else {
-      t += text.charAt(i);
+      t += text.charAt(i)
     }
   }
   setTimeout(function () {
-    var next = document.createElement("p");
-    next.innerHTML = t;
-    next.className = style;
+    var next = document.createElement('p')
+    next.innerHTML = t
+    next.className = style
 
-    before.parentNode.insertBefore(next, before);
+    before.parentNode.insertBefore(next, before)
 
-    window.scrollTo(0, document.body.offsetHeight);
-  }, time);
+    window.scrollTo(0, document.body.offsetHeight)
+  }, time)
 }
 
 function loopLines(name, style, time) {
   name.forEach(function (item, index) {
-    addLine(item, style, index * time);
-  });
+    addLine(item, style, index * time)
+  })
 }
 
 console.log(
-  "%cYou're a Hacker!😠",
-  "color: #04ff00; font-weight: bold; font-size: 24px;"
-);
+  '%cWhat a Hacker!😠\nnow you know my password!',
+  'color: #04ff00; font-weight: bold; font-size: 24px;',
+)
+console.log(
+  "%cPassword: '" + password + "' - do you even know how to use it?🤔",
+  'color: grey',
+)
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Tab") {
-    event.preventDefault();
-    document.getElementById("texter").focus();
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Tab') {
+    event.preventDefault()
+    document.getElementById('texter').focus()
   }
-});
+})
