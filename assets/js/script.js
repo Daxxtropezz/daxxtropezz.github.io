c1cecfb7545a5885590090028a6a5ba3d322f3a9
Daxxtropezz <miraflores.john@gmail.com>
2024-07-12 11:26:21 +0800

    update
    added tippy for references

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,56 @@ menuIcon.onclick = () => {
 	menuIcon.classList.toggle('bx-x');
 	navbar.classList.toggle('active');
 };
+
+// tooltip
+const tooltips = [
+	{
+		selector: '#backtotop',
+		content: 'Back to Top',
+	},
+	{
+		selector: '#ref_hs',
+		content: `Senior Manager Development
+		&#13
+		Contact: (+91) 9650808369`,
+	},
+	{
+		selector: '#ref_dmc',
+		content: 'Director, Core Software Development\nContact: (+63) 9177700877',
+	},
+	{
+		selector: '#ref_at',
+		content: 'I.T. Instructor\nContact: (+63) 999-881-3036',
+	},
+	{
+		selector: '#ref_sff',
+		content: 'UI/UX Designer\nContact: (+63) 993-330-2365',
+	},
+	{
+		selector: '#skillsncerts',
+		content: 'Skills & Certificates',
+	},
+	{
+		selector: '#websitesfin',
+		content: 'Websites & Arts',
+	},
+	{
+		selector: '#contactme',
+		content: 'Contact Me',
+	},
+	{
+		selector: '#terminalbutton',
+		content: 'Terminal Portfolio',
+	},
+	{
+		selector: '#certcommandbutton',
+		content:
+			"Want to see my certificates? Open terminal and type the command 'certificates' to see all.",
+	},
+];
+tooltips.forEach(({ selector, content }) => {
+	tippy(selector, {
+		content: content,
+		followCursor: true,
+	});
+});
