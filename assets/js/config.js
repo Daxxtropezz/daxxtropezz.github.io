0ff0a5c126699235f9f86935bd5e97866cee3065
Daxxtropezz <miraflores.john@gmail.com>
2024-07-14 05:37:14 +0800

    update
    obfuscated file path

---
+++ assets/js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+	//COMPANY LOGOS
+	ASC_LOGO: './assets/images/logos/logo_asc.png',
+	PFH_LOGO: './assets/images/logos/logo_prefabhomes.png',
+	AD_LOGO: './assets/images/logos/logo_azuriadigital.png',
+	COM_LOGO: './assets/images/logos/logo_comelec.png',
+
+	//REFERENCES LOGOS
+	REF_HS: './assets/images/references/references_hs.jpg',
+	REF_DMC: './assets/images/references/references_dmc.jpg',
+	REF_AT: './assets/images/references/references_at.jpg',
+	REF_SF: './assets/images/references/references_sff.jpg',
+
+	//MAIN
+	MAIN_IMG: './assets/images/fbf88b00e9eec9e03c05b10640d74daxxy.jpg',
+};
